18ce07ed6592845de1e38b54a4515cf9b333d5ec
王勇彪 <2499019486@qq.com>
2020-04-22 08:32:37 +0800

    添加了dal层的一些方法

---
+++ DAL/SignAndLogin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using PublicClass;
+
+namespace DAL
+{
+    public class SignAndLogin
+    {
+        /// <summary>
+        /// 注册(判断用户名和手机号是否注册过)
+        /// </summary>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public int Sign(Users u)
+        {
+            int i = 0;
+            if (DapperHelper.NonQuery($"select count(1) from Users where Uname= '{u.Uname}'", null) > 0)
+            {
+                i += 3;
+            }
+            if (DapperHelper.NonQuery($"select count(1) from Users where Utel= '{u.Utel}'", null) > 0)
+            {
+                i+= 5;
+            }
+            if (i<0)
+            {
+                string pwd = EncryptionHelper.Sha1(u.Upwd);//Sha1加密密码
+                i = DapperHelper.NonQuery($"insert into Users values('{u.Uname}','{u.Utname}','{pwd}','{u.Utel}',80,GETDATE())", null);
+            }
+            return i;
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="u"></param>
+        /// <returns></returns>
+        public int Login(Users u)
+        {
+            string pwd = EncryptionHelper.Sha1(u.Upwd);//Sha1加密密码
+            return DapperHelper.NonQuery($"select count(1) from Users where Uname= '{u.Uname}' and Upwd='{pwd}'", null);
+        }
+
+        /// <summary>
+        /// 查询车辆品牌信息用于绑定下拉
+        /// </summary>
+        /// <returns></returns>
+        public List<Brand> GetBrands()
+        {
+            return DapperHelper.Query<Brand>("select * from Brand", null);
+        }
+
+        /// <summary>
+        /// 查询车辆型号信息用于绑定下拉
+        /// </summary>
+        /// <returns></returns>
+        public List<Brand> GetBrands(int bid)
+        {
+            return DapperHelper.Query<Brand>($"select * from Type where Tbid={bid}", null);
+        }
+
+    }
+}
---
+++ PublicClass/DapperHelper.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Configuration;
+using Dapper;
+
+namespace PublicClass
+{
+    /// <summary>
+    /// Dapper 帮助类
+    /// </summary>
+    public static class DapperHelper
+    {
+
+        static string sqlconnection = ConfigurationManager.ConnectionStrings["sqlServer"].ToString();
+
+        private static SqlConnection OpenConnection()
+        {
+
+            SqlConnection connection = new SqlConnection(sqlconnection);  
+            connection.Open();
+            return connection;
+        }
+
+        #region 查询数据
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">sql</param>
+        /// <param name="entity">T</param>
+        /// <returns></returns>
+        public static List<T> Query<T>(string sql, T entity)
+        {
+            List<T> list = new List<T>();
+            using (IDbConnection conn = OpenConnection())
+            {
+
+                list = conn.Query<T>(sql, entity).ToList();
+
+            }
+            return list;
+        }
+
+
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">sql</param>
+        /// <param name="obj">new {}</param>
+        /// <returns></returns>
+        public static List<T> Query<T>(string sql, object obj)
+        {
+            List<T> list = new List<T>();
+            using (IDbConnection conn = OpenConnection())
+            {
+                list = conn.Query<T>(sql, obj).ToList();
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 查询第一行第一列的数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">sql</param>
+        /// <param name="obj">new {}</param>
+        /// <returns></returns>
+        public static T QueryScalar<T>(string sql, object obj)
+        {
+
+            T result = default(T);
+
+            using (IDbConnection conn = OpenConnection())
+            {
+                result = conn.ExecuteScalar<T>(sql, obj);
+            }
+            return result;
+        }
+        #endregion
+
+        #region 执行增，删，改 
+        /// <summary>
+        /// 执行增，删，改 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">sql</param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static int NonQuery<T>(string sql, T entity)
+        {
+            int res = 0;
+            using (IDbConnection conn = OpenConnection())
+            {
+                res = conn.Execute(sql, entity);
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 执行增，删，改 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sql">sql</param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static int NonQuery(string sql, object obj)
+        {
+            int res = 0;
+            using (IDbConnection conn = OpenConnection())
+            {
+                res = conn.Execute(sql, obj);
+            }
+            return res;
+        }
+        #endregion
+
+    }
+}
